0f57ae9270aead9265d2d1d8df06a9be686bb2e0
Mohamed Abdelrasoul <mohamed.ismail@quaraholding.com>
2021-06-25 09:57:26 +0300

    scaffolding the app

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/MainController.java
@@ -0,0 +1,13 @@
+package com.shopme.site;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Component
+public class MainController {
+
+    @GetMapping("")
+    public String viewHomePage(){
+        return "index";
+    }
+}
