4732e7271ccdddd33f2fcd9af21d78494e29d891
Mohamed Abdelrasoul <mohamed.ismail@quaraholding.com>
2021-06-25 14:16:07 +0300

    adding user entity

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -10,50 +10,68 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "roles")
 public class Role {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Column(length = 40, nullable = false, unique = true)
-	private String name;
-	
-	@Column(length = 150, nullable = false)
-	private String description;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	public Role() {
-	}
-	
-	public Role(String name) {
-		this.name = name;
-	}	
-	
-	public Role(String name, String description) {
-		this.name = name;
-		this.description = description;
-	}
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
 
-	public Integer getId() {
-		return id;
-	}
+    @Column(length = 150, nullable = false)
+    private String description;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Role() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Role(Integer id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Role(String name) {
+        this.name = name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+
+        Role role = (Role) o;
+
+        return getId() != null ? getId().equals(role.getId()) : role.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
 }
