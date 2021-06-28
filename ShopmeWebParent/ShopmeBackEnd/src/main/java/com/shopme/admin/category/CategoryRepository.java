7a04561dcba3a546d19c07e4598879168b569458
Mohamed Abdelrasoul <mohamed.ismail@quaraholding.com>
2021-06-28 14:07:25 +0300

    adding paginatoin

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
@@ -3,6 +3,8 @@ package com.shopme.admin.category;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,8 +13,12 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.shopme.common.entity.Category;
 
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
+
     @Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
     public List<Category> findRootCategories(Sort sort);
+
+    @Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
+    public Page<Category> findRootCategories(Pageable pageable);
 
     public Long countById(Integer id);
 
